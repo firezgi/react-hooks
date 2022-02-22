46529aec1a68e02aa2d231a36cffaa0349b2e8d6
Habteab <keshi2017@gmail.com>
2022-02-22 10:19:31 -0800

    feat(app): add hookuseref

---
+++ src/components/Hookuseref.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+function Hookuseref() {
+    const[name,setName]=useState('');
+    const renderCount=useRef(0);
+
+    useEffect(() => {    
+    renderCount.current=renderCount.current+1});
+  return (
+    <>
+    <input value={name} onChange={e=>setName(e.target.value)
+    }/>
+    <div>My name is {name}</div>
+    <div>I rendered {renderCount.current} times</div>
+    </>
+  )
+}
+
+export default Hookuseref
